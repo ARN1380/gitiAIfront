48f30a016256c5b1e1f02b1119f85ae6ddb55a67
ARN1380 <alireza.naghavi0@gmail.com>
2025-02-02 09:19:26 +0330

    add powered by Simazi image to footer and adjust layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import icon from "@/public/assets/images/icon.svg";
 import flowers from "@/public/assets/images/golmol.png";
+import poweredBySimazi from "@/public/assets/images/powered by Simazi.png";
 
 export default function Footer() {
   return (
     <footer className="min-h-[387px] grid grid-cols-4 bg-white shadow-sm">
       <div className="col-span-1 flex flex-col items-center justify-between">
-        <div className="flex gap-[10px] items-center mt-[91px]">
-          <Image src={icon} alt="logo" width={60} height={45} />
-          <h1 className="font-bold text-2xl text-[#332C2B]">Giti Ai</h1>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-[10px] items-center mt-[91px]">
+            <Image src={icon} alt="logo" width={60} height={45} />
+            <h1 className="font-bold text-2xl text-[#332C2B]">Giti Ai</h1>
+          </div>
+          <div>
+            <Image src={poweredBySimazi} alt="Powered by Simazi" />
+          </div>
         </div>
         <div className="self-start">
-            <Image src={flowers} alt="flowers for background" />
+          <Image src={flowers} alt="flowers for background" />
         </div>
       </div>
 
@@ -30,7 +36,7 @@ export default function Footer() {
           <ul className="mt-10 flex flex-col gap-[30px]">
             <li>تولید تصویر</li>
             <li>تولید ویدیو</li>
-            <li>تولید متن</li>            
+            <li>تولید متن</li>
           </ul>
         </div>
         <div>
@@ -38,7 +44,7 @@ export default function Footer() {
           <ul className="mt-10 flex flex-col gap-[30px]">
             <li>سوالات متداول</li>
             <li>تماس بگیرید</li>
-            <li>درباره ما</li>            
+            <li>درباره ما</li>
           </ul>
         </div>
       </menu>
