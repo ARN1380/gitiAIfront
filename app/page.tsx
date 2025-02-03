ff93e6ae0dff13e8ee999fa73417871b1de94061
ARN1380 <alireza.naghavi0@gmail.com>
2025-02-03 14:49:46 +0330

    update toast container position and settings for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   return (
     <>
       <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={true}
         newestOnTop={true}
         closeOnClick={true}
         rtl={true}
